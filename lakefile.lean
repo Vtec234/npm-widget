0ecadbdd40d6d061b70b4626bec41965eedf5894
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-03-08 16:16:45 -0500

    initial

---
+++ lakefile.lean
@@ -0,0 +1,17 @@
+import Lake
+open System Lake DSL
+
+def jsTarget (pkgDir : FilePath) : FileTarget :=
+  let jsFile := pkgDir / "widget/dist/index.js"
+  let srcFiles := inputFileTarget <| pkgDir / "widget/src/index.ts"
+  fileTargetWithDep jsFile srcFiles fun srcFile => do
+    proc { 
+      cmd := "npm"
+      args := #["run", "build"]
+      cwd := some <| pkgDir / "widget"
+    }
+
+package Widget (pkgDir) {
+  extraDepTarget := jsTarget pkgDir |>.withoutInfo
+  -- add configuration options here
+}
