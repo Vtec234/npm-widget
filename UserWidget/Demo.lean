0f061e073e13a5c6647ea6aacacadd6acbaa0d74
E.W.Ayers <edward.ayers@outlook.com>
2022-04-14 15:02:50 -0400

    Implement hashing of code

--- UserWidget/Demo.lean
+++ UserWidget/Demo.lean
@@ -3,18 +3,16 @@ import UserWidget.WidgetCode
 import Mathlib.Util.IncludeStr
 open Lean Server Elab
 
-#check pushInfoTree
-
 def codefn (s : String) := s!"
   import * as React from 'react';
   export default function (props) \{
-    return React.createElement('p', \{}, `{s} ::::::::::: $\{props.pos}`)
+    return React.createElement('p', \{}, `{s} asdf $\{props.pos}`)
   }"
 
-@[staticJS widget1]
+@[staticJS]
 def widget1 : String := codefn "widget1"
 
-@[staticJS widget2]
+@[staticJS]
 def widgetJs : String := include_str "../widget/dist/index.js"
 
 @[staticJS widget3]
@@ -22,33 +20,15 @@ def widget2 : String := codefn "widget3"
 
 open Tactic
 
-#check withInfoContext
-#check saveTacticInfoForToken
-#check String.Pos
-
 syntax (name := widget) "widget!" : tactic
 @[tactic widget]
 def widgetTac : Tactic := fun stx => do
   if let some pos := stx.getPos? then
     let id := `widget1
     let props := Json.mkObj [("pos", pos.byteIdx)]
-    let j := Json.mkObj [
-      ("kind", "widget"),
-      ("id", toJson id),
-      ("props", props)
-    ]
-    CustomInfo.mk stx j
-    |> Info.ofCustomInfo
-    |> pushInfoLeaf
+    Lean.Widget.saveWidget id props stx
 
 theorem asdf : True := by
-  dbg_trace "hello"
   widget!
   trivial
-
-#eval "hello"
-
-def foo := 3
-
-#eval foo
 
