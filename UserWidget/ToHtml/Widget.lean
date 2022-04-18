f87db7b89f4ec27b7122f55edfec0befdf041717
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-04-18 14:56:12 -0400

    feat: add #html command

--- UserWidget/ToHtml/Widget.lean
+++ UserWidget/ToHtml/Widget.lean
@@ -5,34 +5,33 @@ import UserWidget.ToHtml.Html
 @[staticJS]
 def staticHtmlWidget : String := include_str "../../widget/dist/staticHtml.js"
 
-syntax (name := html) "html!" term : tactic
-open Lean Elab Tactic Meta Widget in
-unsafe def htmlTacUnsafe : Tactic := fun stx => do
-  let id := `staticHtmlWidget
+open Lean Elab Widget in
+unsafe def evalHtmlUnsafe (stx : Syntax) : TermElabM Html := do
   let htmlT := mkConst ``Html
-  let htExpr ← elabTerm stx[1] htmlT
-  let decl := Declaration.defnDecl {
-    name        := `_aux_html
-    levelParams := []
-    type        := htmlT
-    value       := htExpr
-    hints       := ReducibilityHints.opaque
-    safety      := DefinitionSafety.safe
-  }
-  let env ← getEnv
-  try
-    addAndCompile decl
-    let ht ← evalConstCheck Html ``Html `_aux_html
+  let htExpr ← Term.elabTerm stx htmlT
+  let htExpr ← Meta.instantiateMVars htExpr
+  Term.evalExpr Html ``Html htExpr
+
+open Lean Elab Widget in
+@[implementedBy evalHtmlUnsafe]
+constant evalHtml : Syntax → TermElabM Html
+
+open Lean Meta Elab Command in
+elab "#html" t:term : command =>
+  runTermElabM none fun _ => do
+    let id := `staticHtmlWidget
+    let ht ← evalHtml t
     let props := Json.mkObj [("html", toJson ht)]
-    Lean.Widget.saveWidget id props stx
-  finally
-    -- Reset the environment to one without the auxiliary config constant
-    setEnv env
+    Lean.Widget.saveWidget id props t
 
-open Lean Elab Tactic Meta Widget in
-@[implementedBy htmlTacUnsafe]
-constant htmlTac : Tactic := fun stx => return ()
+syntax (name := html) "html!" term : tactic
 
--- NB: two attributes on one def doesn't seem to work
+open Lean Elab Tactic in
 @[tactic html]
-def htmlTac' := htmlTac
+def htmlTac : Tactic
+  | `(tactic| html! $t:term) => do
+    let id := `staticHtmlWidget
+    let ht ← evalHtml t
+    let props := Json.mkObj [("html", toJson ht)]
+    Lean.Widget.saveWidget id props t
+  | stx => throwError "Unexpected syntax {stx}."
