a7ba39977de432542ffee0952f045c529ca98fec
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-07-13 23:54:18 -0400

    feat: update build config

--- lakefile.lean
+++ lakefile.lean
@@ -3,15 +3,23 @@ open System Lake DSL
 
 package UserWidget
 
-def tsxTarget (tsxName : String) : FileTarget :=
-  let jsFile := __dir__ / s!"widget/dist/{tsxName}.js"
-  let srcFiles := inputFileTarget <| __dir__ / s!"widget/src/{tsxName}.tsx"
-  fileTargetWithDep jsFile srcFiles fun _srcFile => do
+target packageLock : FilePath :=
+  let packageFile := inputFileTarget <| __dir__ / s!"widget/package.json"
+  let packageLockFile := __dir__ / s!"widget/package-lock.json"
+  fileTargetWithDep packageLockFile packageFile fun _srcFile => do
     proc {
       cmd := "npm"
       args := #["install"]
       cwd := some <| __dir__ / "widget"
     }
+
+def tsxTarget (tsxName : String) : FileTarget :=
+  let jsFile := __dir__ / s!"widget/dist/{tsxName}.js"
+  let deps : Array FileTarget := #[
+    inputFileTarget <| __dir__ / s!"widget/dist/{tsxName}.js",
+    packageLock.target
+  ]
+  fileTargetWithDepArray jsFile deps fun _srcFile => do
     proc {
       cmd := "npm"
       args := #["run", "build"]
