a28d1f1828dfb0a886541630eee82d81cf8b32db
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2022-07-19 19:53:13 -0400

    feat: position diagram embeds

--- widget/src/penrose.tsx
+++ widget/src/penrose.tsx
@@ -1,102 +1,191 @@
-import * as React from "react";
-import * as penrose from "@penrose/core";
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import * as penrose from "@penrose/core"
+import { List } from "@svgdotjs/svg.js"
 
 /** See [here](https://penrose.gitbook.io/penrose/#what-makes-up-a-penrose-program) for explanation. */
 interface PenroseTrio {
-    dsl: string;
-    sty: string;
-    sub: string;
+    dsl: string
+    sty: string
+    sub: string
 }
 
 /** Renders vanilla DOM `Node`s (https://filipmolcik.com/react-children-vanilla-html-element/). */
-function DomElements(props: React.PropsWithChildren<{}>) {
+function DomElements<T>({children, ref: fwdRef}: React.PropsWithChildren<{ref?: React.Ref<T>}>) {
+    // TODO: set the fwdRef here
+    // TODO: maybe use a simpler DomElements, there is only one child
     return <div ref={ref => {
-        if (!ref) return;
-        if (!props.children) return
+        if (!ref) return
+        if (!children) return
         if (ref.firstChild !== null) {
-            ref.replaceChild(props.children as any, ref.firstChild);
+            ref.replaceChild(children as any, ref.firstChild)
         } else {
-            ref.appendChild(props.children as any);
+            ref.appendChild(children as any)
         }
-    }}></div>;
+    }}></div>
 }
 
-const resolvePath: penrose.PathResolver = async (path: string) => {
-    return path
+/** `onSvgDrawn` is called when the diagram has been drawn successfully AND is no longer updating. */
+function PenroseCanvasCore(
+    {dsl, sty, sub, onSvgDrawn}: PenroseTrio & {onSvgDrawn: (_: SVGSVGElement) => void})
+    : JSX.Element
+{
+    const [canvas, setCanvas] = React.useState(<pre>Loading..</pre>)
+
+    const updateDiagramWithError = async (err: string) => {
+        const el = <pre>Penrose error: {err}</pre>
+        setCanvas(el)
+    }
+
+    const updateDiagram = async (state: penrose.PenroseState) => {
+        const nSteps = 500
+        try {
+            state = penrose.stepUntilConvergence(state, nSteps).unsafelyUnwrap()
+            if (!penrose.stateConverged(state))
+                console.warn(`Diagram failed to converge in ${nSteps} steps`)
+            const svg = await penrose.RenderStatic(state, async path => path)
+            const el = <DomElements>{svg}</DomElements>
+            setCanvas(el)
+            onSvgDrawn(svg)
+        } catch (ex: any) {
+            updateDiagramWithError(ex.toString())
+        }
+    }
+
+    React.useEffect(() => {
+        try {
+            const compileRes = penrose.compileTrio({ domain: dsl, style: sty, substance: sub, variation: '' })
+            if (compileRes.isOk()) {
+                penrose.prepareState(compileRes.value)
+                    .then(updateDiagram, ex => updateDiagramWithError(ex.toString()))
+            } else {
+                const err = penrose.showError(compileRes.error)
+                updateDiagramWithError(err)
+            }
+        } catch (ex: any) {
+            updateDiagramWithError(ex.toString())
+        }
+    },
+    // Note: important not to just pass `props` here so the comparison is on trio contents.
+    [dsl, sty, sub, onSvgDrawn]) 
+
+    return canvas
 }
 
 /** Renders an interactive [Penrose](https://github.com/penrose/penrose) diagram with the specified trio.
  * 
- * When `domNodes` is non-empty, for every `(name, elt)` in `domNodes` we locate an object
+ * For every `[name, nd]` in `embedNodes` we locate an object
  * with the same `name` in the substance program, then adjust the style program so that the
- * object's dimensions match those of `elt`, and finally "replace" how `name` is drawn in the SVG
- * with the DOM node `elt`. */
+ * object's dimensions match those of `nd`, and finally "replace" how `name` is drawn in the SVG
+ * with the React node `nd`.
+ * 
+ * This component relies on the length of `embedNodes` never changing! If it changes,
+ * you must re-create it. */
 // TODO link to kc's hack https://github.com/penrose/penrose/issues/1057
-export function PenroseCanvas({dsl, sty, sub, domNodes}: PenroseTrio & {domNodes?: Map<string, Element>}) {
-    const el = <pre>Loading..</pre>;
-    const [canvas, setCanvas] = React.useState(el);
+export function PenroseCanvas(
+        {dsl, sty, sub, embedNodes}: PenroseTrio & {embedNodes: Map<string, React.ReactNode>})
+        : JSX.Element {
+    const [containerDiv, setContainerDiv] = React.useState<HTMLDivElement | null>(null)
 
-    if (domNodes) {
-        for (const [name, elt] of domNodes.entries()) {
-            if (!elt) continue
-            const rect = elt.getBoundingClientRect();
-            console.log(name, '=>', elt, '(', rect.width, 'x', rect.height, ')');
-            sty = sty +
-`
-Targettable \`${name}\` {
-   override \`${name}\`.textBox.width = ${rect.width}
-   override \`${name}\`.textBox.height = ${rect.height}
-}
-`
-        }
+    interface EmbedData {
+        elt: HTMLDivElement | undefined
+        portal: React.ReactPortal
     }
-
-    const updateDiagramWithError = async (err: any) => {
-        const el = <pre>Penrose error: {err.toString()}</pre>;
-        setCanvas(el);
-    };
-
-    const updateDiagram = async (state: penrose.PenroseState) => {
-        let i = 0;
-        const maxSteps = 10;
-        // TODO(WN): the convergence to interactivity is not very useful, just go with a static diagram
-        do {
-            try {
-                const svg = await penrose.RenderStatic(state, resolvePath);
-                const el = <DomElements>{svg}</DomElements>;
-                setCanvas(el);
-
-                state = penrose.stepUntilConvergence(state, 100).unsafelyUnwrap()
-            } catch (ex: any) {
-                updateDiagramWithError(ex.toString());
-            }
-            i += 1;
-        } while (!penrose.stateConverged(state) && i < maxSteps);
-        if (i === maxSteps) {
-            updateDiagramWithError(`Diagram failed to converge in ${maxSteps*100} steps. Inconsistent constraints?`);
-        } else {
-            const svg = await penrose.RenderInteractive(state, updateDiagram, resolvePath);
-            const el = <DomElements>{svg}</DomElements>;
-            setCanvas(el);
-        }
-    };
+    const [embeds, setEmbeds] = React.useState<Map<string, EmbedData>>(new Map())
 
     React.useEffect(() => {
-        try {
-            const compileRes = penrose.compileTrio({ domain: dsl, style: sty, substance: sub, variation: '' });
-            if (compileRes.isOk()) {
-                penrose.prepareState(compileRes.value)
-                    .then(updateDiagram, updateDiagramWithError);
-            } else {
-                const err = penrose.showError(compileRes.error);
-                updateDiagramWithError(err);
+        console.log("embedNodes eff", embedNodes)
+        if (!embedNodes) return
+        if (!containerDiv) return
+        const newEmbeds: Map<string, EmbedData> = new Map()
+        for (const [name, nd] of embedNodes) {
+            const div = <div className="dib absolute" ref={newDiv => {
+                if (!newDiv) return
+                console.log('div eff', newDiv)
+                setEmbeds(embeds => {
+                    const newEmbeds: Map<string, EmbedData> = new Map()
+                    let changed = false
+                    for (const [eName, data] of embeds) {
+                        if (eName === name && data.elt !== newDiv) {
+                            changed = true
+                            newEmbeds.set(eName, {...data, elt: newDiv})
+                        } else newEmbeds.set(eName, data)
+                    }
+                    return changed ? newEmbeds : embeds
+                })
+            }}>{nd}</div>
+            const portal = ReactDOM.createPortal(div, containerDiv, name)
+            const data: EmbedData = {
+                elt: undefined,
+                portal
             }
-        } catch (ex: any) {
-            updateDiagramWithError(ex.toString());
+            newEmbeds.set(name, data)
         }
-    },
-    // Note: important not to just pass `props` here so the comparison is on contents.
-    [dsl, sty, sub]); 
+        setEmbeds(newEmbeds)
+    // `deps` must have constant size so we can't do a deeper comparison
+    }, [embedNodes, containerDiv])
 
-    return canvas;
+    let dim = 400
+    if (containerDiv) {
+        const rect = containerDiv.getBoundingClientRect()
+        dim = Math.ceil(Math.max(400, rect.width))
+    }
+
+    sty = sty +
+`
+canvas {
+    width = ${dim}
+    height = ${dim}
+}
+`
+
+    for (const [name, {elt}] of embeds) {
+        if (!elt) continue
+        const rect = elt.getBoundingClientRect()
+        // console.log(name, '=>', elt, '(', rect.width, 'x', rect.height, ')')
+        sty = sty +
+`
+Targettable \`${name}\` {
+override \`${name}\`.textBox.width = ${Math.ceil(rect.width)}
+override \`${name}\`.textBox.height = ${Math.ceil(rect.height)}
+}
+`
+    }
+
+    // Store the boxes that we can draw interactive elements over
+    const [diagramBoxes, setDiagramBoxes] = React.useState<Map<string, Element>>()
+    const onSvgDrawn = React.useMemo(() => {
+        console.log('recompute onSvgDrawn')
+        return (svg: SVGSVGElement) => {
+            const diagramBoxes = new Map<string, Element>()
+            for (const gElt of svg.querySelectorAll("g, rect")) {
+                if (!gElt.textContent) continue
+                const gps = gElt.textContent.match(/`(\w+)`.textBox/)
+                if (!gps) continue
+                const name = gps[1]
+                diagramBoxes.set(name, gElt)
+            }
+            setDiagramBoxes(diagramBoxes)
+    }}, [])
+
+    React.useEffect(() => {
+        if (!diagramBoxes) return
+        if (!containerDiv) return
+        console.log('repositioning')
+        for (const [name, gElt] of diagramBoxes) {
+            const embed = embeds.get(name)
+            if (!embed) continue
+            const divElt = embed.elt
+            if (!divElt) continue
+            const gRect = gElt.getBoundingClientRect(),
+                  containerRect = containerDiv.getBoundingClientRect()
+            divElt.style.top = `${gRect.top - containerRect.top}px`
+            divElt.style.left = `${gRect.left - containerRect.left}px`
+        }
+    }, [diagramBoxes, embeds, containerDiv])
+
+    return <div className="relative" ref={setContainerDiv}>
+        <PenroseCanvasCore dsl={dsl} sty={sty} sub={sub} onSvgDrawn={onSvgDrawn} />
+        {Array.from(embeds.values()).map(({portal}) => portal)}
+    </div>
 }
--- widget/src/squares.tsx
+++ widget/src/squares.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { RpcSessions } from '@lean4/infoview/infoview/rpcSessions';
-import { DocumentPosition, InteractiveCode, useAsync, RpcContext, CodeWithInfos } from '@lean4/infoview';
+// import { RpcSessions } from '@lean4/infoview/infoview/rpcSessions';
+import { DocumentPosition, InteractiveCode, useAsync, RpcContext, CodeWithInfos, RpcSessions } from '@lean4/infoview';
 // TODO: can we make relative imports work? We get
 // Unable to resolve specifier '@lean4/infoview/infoview/rpcInterface' imported from blob:vscode-webview://13bu9k5qafb04m5f7smuipoa2rui17ms4t3ilmibfnnsd5h3tg7k/f47b074c-568b-4501-a914-26d9a1ef1194
 // import { CodeWithInfos_registerRefs } from '@lean4/infoview/infoview/rpcInterface';
@@ -21,49 +21,61 @@ interface DiagramData {
 }
 
 function CommSquare({pos, diag}: {pos: DocumentPosition, diag: DiagramData}): JSX.Element {
-    return <PenroseCanvas dsl={commutativeDsl} sty={commutativeSty} sub={commutativeSquareSub} />
+    const [A,B,C,D] = diag.objs
+    const [f,g,h,i] = diag.homs
+
+    const mkElt = (fmt: CodeWithInfos): JSX.Element =>
+        <div className="pa2">
+            <InteractiveCode pos={pos} fmt={fmt} />
+        </div>
+
+    const [embedNodes, setEmbedNodes] = React.useState<Map<string, React.ReactNode>>()
+    React.useEffect(() => {
+        const embedNodes = new Map()
+            .set("A", mkElt(A))
+            .set("B", mkElt(B))
+            .set("C", mkElt(C))
+            .set("D", mkElt(D))
+            .set("f", mkElt(f))
+            .set("g", mkElt(g))
+            .set("h", mkElt(h))
+            .set("i", mkElt(i))
+        setEmbedNodes(embedNodes)
+    }, [pos.uri, pos.line, pos.character, A, B, C, D, f, g, h, i])
+
+    if (!embedNodes) return <></>
+    else return <PenroseCanvas
+        dsl={commutativeDsl} sty={commutativeSty} sub={commutativeSquareSub}
+        embedNodes={embedNodes}
+    />
 }
 
 function CommTriangle({pos, diag}: {pos: DocumentPosition, diag: DiagramData}): JSX.Element {
     const [A,B,C] = diag.objs
     const [f,g,h] = diag.homs
 
-    const mkPortal = (fmt: CodeWithInfos): [React.RefObject<HTMLDivElement>, React.ReactPortal] => {
-        const ref = React.useRef<HTMLDivElement>(null)
-        const codeA = <div className="dib pa2" ref={ref}>
+    const mkElt = (fmt: CodeWithInfos): JSX.Element =>
+        <div className="pa2">
             <InteractiveCode pos={pos} fmt={fmt} />
         </div>
-        const elt = ReactDOM.createPortal(codeA, document.body)
-        return [ref, elt];
-    }
 
-    const [refA, eltA] = mkPortal(A)
-    const [refB, eltB] = mkPortal(B)
-    const [refC, eltC] = mkPortal(C)
-    const [reff, eltf] = mkPortal(f)
-    const [refg, eltg] = mkPortal(g)
-    const [refh, elth] = mkPortal(h)
+    const [embedNodes, setEmbedNodes] = React.useState<Map<string, React.ReactNode>>()
+    React.useEffect(() => {
+        const embedNodes = new Map()
+            .set("A", mkElt(A))
+            .set("B", mkElt(B))
+            .set("C", mkElt(C))
+            .set("f", mkElt(f))
+            .set("g", mkElt(g))
+            .set("h", mkElt(h))
+        setEmbedNodes(embedNodes)
+    }, [pos.uri, pos.line, pos.character, A, B, C, f, g, h])
 
-    const domNodes = new Map()
-        .set("A", refA.current)
-        .set("B", refB.current)
-        .set("C", refC.current)
-        .set("f", reff.current)
-        .set("g", refg.current)
-        .set("h", refh.current)
-
-    return <>
-        <PenroseCanvas dsl={commutativeDsl} sty={commutativeSty} sub={commutativeTriangleSub}
-            domNodes={domNodes} />
-        {/* I think we need to "return" the portals to make sure they get rendered.
-        Maybe PenroseCanvas can take them as args and return instead. */}
-        {eltA}
-        {eltB}
-        {eltC}
-        {eltf}
-        {eltg}
-        {elth}
-    </>
+    if (!embedNodes) return <></>
+    else return <PenroseCanvas
+        dsl={commutativeDsl} sty={commutativeSty} sub={commutativeTriangleSub}
+        embedNodes={embedNodes}
+    />
 }
 
 function DiagramData_registerRefs(rs: RpcSessions, pos: DocumentPosition, sd: DiagramData) {
@@ -79,18 +91,25 @@ async function getCommutativeDiagram(rs: RpcSessions, pos: DocumentPosition): Pr
 
 export default function({pos}: {pos: DocumentPosition}): React.ReactNode {
     const rs = React.useContext(RpcContext)
-    const [status, diag, err] = useAsync(() => getCommutativeDiagram(rs, pos), [pos.uri, pos.line, pos.character])
+    const [status, diag, err] = useAsync(
+        () => getCommutativeDiagram(rs, pos),
+        [pos.uri, pos.line, pos.character])
 
+    let msg = <></>
     if (status === 'pending')
-        return <>Loading...</>
+        msg = <>Loading...</>
     else if (status === 'rejected')
-        return <>Error: {JSON.stringify(err)}</>
-    else if (diag && diag.kind === 'square')
-        return <CommSquare pos={pos} diag={diag} />
-    else if (diag && diag.kind === 'triangle')
-        return <CommTriangle pos={pos} diag={diag} />
-    else
-        // TODO: Why does this case occur initially?
-        console.error('Unexpected data ', diag)
-        return <>Unexpected data: {diag}; error?: {err}</>
+        msg = <>Error: {JSON.stringify(err)}</>
+    else if (status === 'fulfilled' && !diag)
+        msg = <>No diagram.</>
+
+    // We keep the diagrams alive to avoid a re-render when the cursor moves
+    // to a position containing the same diagram.
+    return <>
+        {msg}
+        {diag && diag.kind === 'square' &&
+            <CommSquare pos={pos} diag={diag} />}
+        {diag && diag.kind === 'triangle' &&
+            <CommTriangle pos={pos} diag={diag} /> }
+    </>
 }
