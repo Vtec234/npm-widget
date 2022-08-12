e7ead8e20e090bae68728d169f4e38de652d4d41
E.W.Ayers <edward.ayers@outlook.com>
2022-08-12 11:08:01 +0100

    fix plotting

--- UserWidget/Demos/Plot.lean
+++ UserWidget/Demos/Plot.lean
@@ -7,14 +7,14 @@ open Std
 open Lean.Widget in
 @[widget]
 def ppp : UserWidgetDefinition where
-  name := "This is a plot"
+  name := "Function plot"
   javascript := include_str ".." / ".." / "widget" / "dist" / "plot.js"
 
 def fn (x : Float): Float :=
-  2 + x - x * x + 2 * (x * x * x)
+   1.2 * x - 4 * x * x + 3 * (x * x * x) + 0.1 * (x * 20).sin
 
-def data := List.range 10 |> List.map Float.ofNat |>.toArray
-      |> Array.map (fun x => x / 10)
+def data := List.range 101 |> List.map Float.ofNat |>.toArray
+      |> Array.map (fun x => x / 100)
       |> Array.map (fun x => json% {x: $(x), y: $(fn x)})
       |> Lean.Json.arr
 
@@ -22,4 +22,7 @@ def props := json% {data: $(data)}
 
 #eval props
 
+-- tip: try pinning the widget in the infoview
+-- and then editing the 'fn' above to watch the
+-- plot animate
 #widget ppp props
